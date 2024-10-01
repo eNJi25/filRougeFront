bdcfcf2d475afa8a946a0ed5f83113220be89162
eNJi25 <nfischer.contact@gmail.com>
2024-10-01 09:10:38 +0200

    feat: function validateForm for signup

--- js/signup.js
+++ js/signup.js
@@ -27,8 +27,13 @@ function validateForm() {
   const nomOk = validateRequired(inputNom);
   const prenomOk = validateRequired(inputPrenom);
   const mailOk = validateMail(inputEmail);
+  const passwordOk = validatePassword(inputPassword);
+  const validatePasswordOk = validateConfirmationPassword(
+    inputPassword,
+    inputValidatePassword
+  );
 
-  if (nomOk && prenomOk && mailOk) {
+  if (nomOk && prenomOk && mailOk && passwordOk && validatePasswordOk) {
     btnValidationInscription.disabled = false;
   } else {
     btnValidationInscription.disabled = true;
@@ -57,6 +62,33 @@ function validateMail(input) {
   } else {
     input.classList.remove("is-valid");
     input.classList.add("is-invalid");
+    return false;
+  }
+}
+
+function validatePassword(input) {
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+  const passwordUser = input.value;
+  if (passwordUser.match(passwordRegex)) {
+    input.classList.add("is-valid");
+    input.classList.remove("is-invalid");
+    return true;
+  } else {
+    input.classList.remove("is-valid");
+    input.classList.add("is-invalid");
+    return false;
+  }
+}
+
+function validateConfirmationPassword(inputPwd, inputConfirmPwd) {
+  if (inputPwd.value == inputConfirmPwd.value) {
+    inputConfirmPwd.classList.add("is-valid");
+    inputConfirmPwd.classList.remove("is-invalid");
+    return true;
+  } else {
+    inputConfirmPwd.classList.remove("is-valid");
+    inputConfirmPwd.classList.add("is-invalid");
     return false;
   }
 }
